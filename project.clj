455091d70a90c8fe4b4c325d8711362b04c8f45b
Chris Zheng <z@caudate.me>
2017-02-16 09:45:46 +1100

    added jgit 4.6

--- project.clj
+++ project.clj
@@ -45,7 +45,7 @@
                  [markdown-clj/markdown-clj "0.9.94"]
                  [hiccup/hiccup "1.0.5"]
                  [stencil/stencil "0.5.0"]
-                 [org.eclipse.jgit "4.5.1-sgk"]
+                 [org.eclipse.jgit "4.6.0.201612231935-r"]
                  [com.github.dblock/oshi-core "3.3"]]
   
   :publish {:theme  "stark"
