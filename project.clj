6b3b31e10cbbe5af034eea540b1491f53eab88ac
Chris Zheng <czheng@atlassian.com>
2017-03-14 17:15:46 +1100

    upgraded version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject im.chit/lucid "1.3.6"
+(defproject im.chit/lucid "1.3.7"
   :description "tools for code clarity"
   :url "https://www.github.com/zcaudate/lucidity"
   :license {:name "The MIT License"
